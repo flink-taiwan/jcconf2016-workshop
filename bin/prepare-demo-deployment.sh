ddf6c8bfa5f5df56267b00b14f99d8ae719ec8e6
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2016-10-07 16:16:51 +0800

    Don't use https schemes

--- bin/prepare-demo-deployment.sh
+++ bin/prepare-demo-deployment.sh
@@ -9,14 +9,14 @@ fi
 cd "$PROJECT_ROOT/deploy"
 
 if [ ! -d "$PROJECT_ROOT/deploy/influxdb" ]; then
-    wget https://dl.influxdata.com/influxdb/releases/influxdb-1.0.0_linux_amd64.tar.gz
+    wget http://dl.influxdata.com/influxdb/releases/influxdb-1.0.0_linux_amd64.tar.gz
     tar -xvf influxdb-1.0.0_linux_amd64.tar.gz
     mv influxdb-1.0.0-1 influxdb
     rm influxdb-1.0.0_linux_amd64.tar.gz
 fi
 
 if [ ! -d "$PROJECT_ROOT/deploy/grafana" ]; then
-    wget https://grafanarel.s3.amazonaws.com/builds/grafana-3.1.1-1470047149.linux-x64.tar.gz
+    wget http://grafanarel.s3.amazonaws.com/builds/grafana-3.1.1-1470047149.linux-x64.tar.gz
     tar -xvf grafana-3.1.1-1470047149.linux-x64.tar.gz
     mv grafana-3.1.1-1470047149 grafana
     rm grafana-3.1.1-1470047149.linux-x64.tar.gz
